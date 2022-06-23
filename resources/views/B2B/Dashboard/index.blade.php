16e53dcd0c80cf8616ccf5be9b96b1ce66b5e91a
DESKTOP-N5BRLTN\aaa <asd356265@gmail.com>
2022-06-23 15:30:58 +0800

    加上loading畫面

--- resources/views/B2B/Dashboard/index.blade.php
+++ resources/views/B2B/Dashboard/index.blade.php
@@ -108,6 +108,7 @@
         })
 
         function rebootHotspot(MAC){
+            var modal =  UIkit.modal.blockUI('<div class=\'uk-text-center\'>Loading...<br/><br/><div class=\"md-preloader uk-text-center\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" height=\"48\" width=\"48\" viewbox=\"0 0 75 75\"><circle cx=\"37.5\" cy=\"37.5\" r=\"33.5\" stroke-width=\"4\"/></svg></div></div>');
             $.ajax({
                 type: "POST",
                 url:"http://192.168.150.163:49880/rebootHotspot",
@@ -115,6 +116,7 @@
                     mac: MAC 
                 },
                 success: function(response){
+                    modal.hide();
                     // alert(response);
                     if(response.status == 0){
                         alert('成功');
@@ -123,6 +125,7 @@
                     }
                 },
                 error : function(xhr, ajaxOptions, thrownError){
+                    modal.hide();
                     canSendGift = true;
                     switch (xhr.status) {
                         case 422:
